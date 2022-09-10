4ccd8cc6870f29f1d2d9b94d92f36848526275c1
maars202 <85531823+maars202@users.noreply.github.com>
2022-09-11 00:14:55 +0800

    styling changes multimint

--- src/pages/Toggle.tsx
+++ src/pages/Toggle.tsx
@@ -49,13 +49,14 @@ const getItemStyle = (isDragging, draggableStyle) => ({
   margin: `0 0 ${grid}px 0`,
 
   // change background colour if dragging
-  background: isDragging ? "lightgreen" : "grey",
+  // background: isDragging ? "lightgreen" : "grey",
+  background: isDragging ? "lightgreen" : "",
 
   // styles we need to apply on draggables
   ...draggableStyle
 });
 const getListStyle = isDraggingOver => ({
-  background: isDraggingOver ? "lightblue" : "lightgrey",
+  background: isDraggingOver ? "lightblue" : "",
   padding: grid,
   width: 250
 });
@@ -172,42 +173,85 @@ console.log("existing selectedImages: ", selectedImages?.files)
       >
         Add new item
       </button> */}
-      <div style={{ display: "flex" }}>
-        <DragDropContext onDragEnd={onDragEnd}>
-          {state.map((el, ind) => (
-            <Droppable key={ind} droppableId={`${ind}`}>
+      <div style={{ display: "flex" }} className="">
+        <DragDropContext onDragEnd={onDragEnd} class="bg-orange-300" style={{backgroundColor: "purple"}}>
+          {state.map((el, ind) => (<div>
+            <div className='grid justify-items-center p-4'>
+              {/* <div class="font-bold text-sky-500">Tier {ind}</div> */}
+              <h1 class="font-extrabold text-transparent text-2xl bg-clip-text bg-gradient-to-r from-sky-400 to-purple-600 hover:underline hover:scale-125">
+              Level {ind + 1}</h1>
+              </div>
+            <Droppable key={ind} droppableId={`${ind}`} class="bg-green-300" >
               {(provided, snapshot) => (
                 <div
                   ref={provided.innerRef}
                   style={getListStyle(snapshot.isDraggingOver)}
+                  // style={getListStyle({background: "orange", padding: 1, width:1})}
                   {...provided.droppableProps}
+                  // style="w-20"
                 >
                   {el.map((item, index) => (
                     <Draggable
                       key={item.id}
                       draggableId={item.id}
                       index={index}
+                      class="bg-yellow-200"
                     >
                       {(provided, snapshot) => (
                         <div
                           ref={provided.innerRef}
                           {...provided.draggableProps}
                           {...provided.dragHandleProps}
-                          style={getItemStyle(
-                            snapshot.isDragging,
-                            provided.draggableProps.style
-                          )}
+                          // style={getItemStyle(
+                          //   snapshot.isDragging,
+                          //   provided.draggableProps.style
+                          // )}
+                          className=''
                         >
-                          <div className='flex flex-col'
+                          <div className='flex flex-col rounded-lg'
                             style={{
                               display: "flex",
                               justifyContent: "space-around"
                             }}
-                          >
-                            <div className='grid justify-items-center'>{item.file.name}</div>
                             
-                            <img src={URL.createObjectURL(item.file)}/>
-                            <button
+                          >
+                            {/* <div className='grid justify-items-center text-orange-600'>{item.file.name}</div> */}
+                            
+                            {/* <img src={URL.createObjectURL(item.file)} className="rounded-lg hover:scale-105"></img> */}
+
+
+                            <div class="my-2 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+    <a href="#">
+        <img class="rounded-t-lg" src={URL.createObjectURL(item.file)} alt=""/>
+    </a>
+    <div class="p-5 grid justify-items-center">
+        {/* <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Read more
+            <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </a> */}
+        <button type="button"
+                                      onClick={() => {
+                                        const newState = [...state];
+                                        newState[ind].splice(index, 1);
+                                        setState(
+                                          newState.filter(group => group.length)
+                                        );
+                                      }}
+                                      class="inline-flex items-center mt-2 pl-2 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-md">
+                                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                  </svg>
+                                  
+                                  </button>
+    </div>
+</div>
+
+
+                            {/* <button
                               type="button"
                               onClick={() => {
                                 const newState = [...state];
@@ -218,7 +262,25 @@ console.log("existing selectedImages: ", selectedImages?.files)
                               }}
                             >
                               delete
-                            </button>
+                            </button> */}
+
+
+                            {/* <div class="grid justify-items-end">
+                                <button type="button"
+                                      onClick={() => {
+                                        const newState = [...state];
+                                        newState[ind].splice(index, 1);
+                                        setState(
+                                          newState.filter(group => group.length)
+                                        );
+                                      }}
+                                      class="inline-flex items-center mt-2 pl-2 py-2 bg-red-600 hover:bg-red-700 text-white text-sm font-medium rounded-md">
+                                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                  </svg>
+                                  
+                                  </button>
+                              </div> */}
                           </div>
                         </div>
                       )}
@@ -228,6 +290,7 @@ console.log("existing selectedImages: ", selectedImages?.files)
                 </div>
               )}
             </Droppable>
+            </div>
           ))}
         </DragDropContext>
       </div>
